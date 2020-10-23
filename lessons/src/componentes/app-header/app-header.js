dc71c730dd1c34b5b168dc4e67e5a672c86f4798
AlexanderPistiulha <olexanderfreelance@gmail.com>
2020-10-23 20:35:23 +0300

    added basic markup and modularity

--- lessons/src/componentes/app-header/app-header.js
+++ lessons/src/componentes/app-header/app-header.js
@@ -1,1 +1,14 @@
 import React from "react";
+
+import "./app-header.css";
+
+const AppHeader = () => {
+  return (
+    <div className="app-header d-flex">
+      <h1>Olexander Pistiulha</h1>
+      <h2>5 аписей, из них понравилось 0</h2>
+    </div>
+  );
+};
+
+export default AppHeader;
---
+++ lessons/src/componentes/post-list-item/post-list-item.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import "./post-list-item.css";
+
+const PostListItem = () => {
+  return (
+    <li className="app-list-item d-flex justify-content-between">
+      <span className="app-list-item-label">Helloworld</span>
+      <div className="d-flex justify-content-center align-items-center">
+        <button type="submit" className="btn-star btn-sm">
+          <i className="fa fa-stat"></i>
+        </button>
+        <button type="submit" className="btn-trash btn-sm">
+          <i className="fa fa-trash-o"></i>
+        </button>
+        <i className="fa fa-heart"></i>
+      </div>
+    </li>
+  );
+};
+
+export default PostListItem;
