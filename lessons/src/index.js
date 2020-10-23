d70592c07465747d4000cb3b531226dc1fa18e72
AlexanderPistiulha <olexanderfreelance@gmail.com>
2020-10-23 12:56:47 +0300

    creating a project structure

--- lessons/src/index.js
+++ lessons/src/index.js
@@ -1,31 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-// import App from './App';
-// import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./componentes/app/app";
 
-
-const Header = () => {
-    return <h2>hello hello</h2>
-}
-
-const Field = () => {
-    return <input type="text" placeholder="type here" />
-}
-
-const Btn = ()=> {
-    const text = "log in";
-    const  loged = true;
-    return <button>{loged ? "enter your log in" : text}</button>}
-
-const App = ()=> {
-    return(
-        <div>
-            <Header/>
-            <Field/>
-            <Btn/>
-        </div>
-    )
-}
-
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
