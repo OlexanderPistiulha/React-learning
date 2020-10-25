04cb44826d05426fc52ec54928af6c31ce604de6
AlexanderPistiulha <olexanderfreelance@gmail.com>
2020-10-25 12:27:01 +0200

    learned state and event handlers

--- lessons/src/componentes/app/app.js
+++ lessons/src/componentes/app/app.js
@@ -7,8 +7,17 @@ import SearchPanel from "../search-panel/search-panel";
 import PostStatusFilter from "../post-status-filter/post-status-filter";
 import PostList from "../post-list/post-list";
 import PostAddForm from "../post-add-form/post-add-form";
+//import PostListItem from "../post-list-item/post-list-item";
 
 const App = () => {
+  //  data what we take  from imaginative server
+  const data = [
+    { label: "something one", important: true, id: "eknelnel" },
+    { label: "something two", important: false, id: "eknelnedel" },
+    { label: "something three", important: false, id: "ekededenelnel" },
+  ];
+  //  -------------------------------------------
+
   return (
     <div className="app">
       <AppHeader />
@@ -16,7 +25,7 @@ const App = () => {
         <SearchPanel />
         <PostStatusFilter />
       </div>
-      <PostList />
+      <PostList posts={data} />
       <PostAddForm />
     </div>
   );
--- lessons/src/componentes/post-list/post-list.js
+++ lessons/src/componentes/post-list/post-list.js
@@ -4,14 +4,16 @@ import "./post-list.css";
 
 import PostListItem from "../post-list-item/post-list-item";
 
-const PostList = () => {
-  return (
-    <ul className="app-list list-group">
-      <PostListItem />
-      <PostListItem />
-      <PostListItem />
-    </ul>
-  );
+const PostList = ({ posts }) => {
+  const elements = posts.map((item) => {
+    const { id, ...itemProps } = item;
+    return (
+      <li key={id} className="list-gtoup-item">
+        <PostListItem {...itemProps} />
+      </li>
+    );
+  });
+  return <ul className="app-list list-group">{elements}</ul>;
 };
 
 export default PostList;
